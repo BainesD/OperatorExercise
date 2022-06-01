4becf13983f102caa40c81cf9217f3dffa3ce79b
Baines Deal <bainesdeal@gmail.com>
2022-06-01 09:14:40 -0500

    Initial Commit

---
+++ OperatorExercise/OperatorExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine("What is the radius of your circle?");
+            var r = double.Parse(Console.ReadLine());
+            var area = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine($"The area of a circle with the radius of {r} is {area}");
+
+        }
+    }
+}
